bdd5c8f71a9f6cb8ebb51fc20a045e81d4fc1442
Linus Sehn <37184648+linozen@users.noreply.github.com>
2024-03-13 08:59:37 +0100

    feat(plugin): citations (#984)
    * feat: add rehype-citations
    
    * feat: add citations transformer plugin
    
    * feat: add rehype-rewrite
    
    * feat: add csl option and add no-popover to citation links
    
    * revert: add rehype-rewrite
    
    04b2692 'feat: add rehype-rewrite'
    
    * feat: use existing package for html manipulation
    
    * fix: remove `console.log()`

--- quartz/plugins/transformers/citations.ts
+++ quartz/plugins/transformers/citations.ts
@@ -1,14 +1,20 @@
 import rehypeCitation from "rehype-citation"
+import { PluggableList } from "unified"
+import { visit } from "unist-util-visit"
 import { QuartzTransformerPlugin } from "../types"
 
 export interface Options {
   bibliographyFile: string
   suppressBibliography: boolean
+  linkCitations: boolean
+  csl: string
 }
 
 const defaultOptions: Options = {
-  bibliographyFile: "./exocortex.bib",
+  bibliographyFile: "./bibliography.bib",
   suppressBibliography: false,
+  linkCitations: false,
+  csl: "apa",
 }
 
 export const Citations: QuartzTransformerPlugin<Partial<Options> | undefined> = (userOpts) => {
@@ -16,18 +22,31 @@ export const Citations: QuartzTransformerPlugin<Partial<Options> | undefined> = 
   return {
     name: "Citations",
     htmlPlugins() {
-      return [
-        [
-          rehypeCitation,
-          {
-            bibliography: opts.bibliographyFile,
-            suppressBibliography: opts.suppressBibliography,
-            // Until popovers are implemented to not show for citation links, I
-            // don't want to link citations
-            linkCitations: false,
-          },
-        ],
-      ]
+      const plugins: PluggableList = []
+
+      // Add rehype-citation to the list of plugins
+      plugins.push([
+        rehypeCitation,
+        {
+          bibliography: opts.bibliographyFile,
+          suppressBibliography: opts.suppressBibliography,
+          linkCitations: opts.linkCitations,
+        },
+      ])
+
+      // Transform the HTML of the citattions; add data-no-popover property to the citation links
+      // using https://github.com/syntax-tree/unist-util-visit as they're just anochor links
+      plugins.push(() => {
+        return (tree, _file) => {
+          visit(tree, "element", (node, index, parent) => {
+            if (node.tagName === "a" && node.properties?.href?.startsWith("#bib")) {
+              node.properties["data-no-popover"] = true
+            }
+          })
+        }
+      })
+
+      return plugins
     },
   }
 }
